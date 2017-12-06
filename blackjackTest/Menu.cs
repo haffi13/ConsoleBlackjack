de4a4810d7a4f9c94b74440938f3d3c106be09e1
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2017-12-06 17:15:49 +0100

    Added menu class

---
+++ blackjackTest/Menu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjackTest
+{
+    class Menu
+    {
+        Game game = new Game();
+        Decide decide = new Decide();
+        public void MainMenu()
+        {
+            bool running = true;
+            Console.WriteLine("Press 1 to start game");
+            while (running)
+                switch (GetUserInput())
+                {
+                    case 1: StartGame(); break;
+                    case 0: running = false; break;
+                    default: Console.WriteLine("Wrong input"); break;
+                }
+        }
+        public void StartGame()
+        {
+            decide.Decision();
+        }
+        public int GetUserInput()
+        {
+            bool invalidSelection = true;
+            int iInput = 0;
+            while (invalidSelection)
+            {
+                string sInput = string.Empty;
+                sInput = Console.ReadLine();
+                bool validSelection = Int32.TryParse(sInput, out iInput);
+                if (validSelection)
+                {
+                    invalidSelection = false;
+                    iInput = Int32.Parse(sInput);
+                }
+                else
+                {
+                    Console.WriteLine("Selection is not valid.");
+                }
+            }
+            return iInput;
+        }
+    }
+}
